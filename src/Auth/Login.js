6d91df5e4b850f13cb129cbd79507ab9d6752fe9
Pranjal <pranjaljain0697@gmail.com>
2020-05-09 18:32:35 +0530

    changes

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,57 +1,70 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Redirect } from 'react-router-dom'
 import styled from 'styled-components'
-import { InputGroup,FormControl,Form } from "react-bootstrap";
+import { InputGroup,FormControl,Form,Button } from "react-bootstrap";
 
 function Login() {
 
     const [validated, setValidated] = useState(false);
+    const [RedirectVar, setRedirectVar] = useState(false)
+    const [SignUpRedir, setSignUpRedir] = useState(false)
+    const [PasswordValidation, setPasswordValidation] = useState(false)
+
+    
 
     const handleSubmit = (event) => {
         const form = event.currentTarget;
         if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        else{
+            setRedirectVar(true)
         }
         setValidated(true);
     };
 
-    const  handlePassword=(event)=>{
-        var pass = event.target.value;
-        var reg = /^[A-Z]*$/;
-        var test = reg.test(pass);
-        if (test) {
-           alert('pass');
-           this.setState({value: pass});
-        }else{
-          alert('fail');
-        }        
+   const setValidationState=()=>{
+       if(PasswordValidation===true&&validated===true)
+        return true
    }
+
+   const renderRedirect = () => {
+        if (RedirectVar) {
+            return <Redirect to='/dashboard' />
+        }
+        else if(SignUpRedir){
+            return <Redirect to='/sign-up'/>
+        }
+    }
+
+
     return (
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                <h3>Sign In</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email"  />
-                </div>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            {renderRedirect()}
+            <h3>Sign In</h3>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control required type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" onChange={(e)=>handlePassword(e)} />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="button" className="btn btn-custom btn-block" onClick={(e)=>handleSubmit(e)}>Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control required type="password" placeholder="Password" />
+            </Form.Group>
+            <Button type="submit" disabled={setValidationState()} className="btn btn-custom btn-block">
+                Submit
+            </Button>
+            <p className="forgot-password text-right">
+                    New User 
+                    <Button type="button" disabled={setValidationState()} className="btn btn-custom" onClick={()=>{setSignUpRedir(true)}}>
+                        Sign up
+                    </Button>
+            </p>
             </Form>
     )
 }
--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -1,37 +1,90 @@
-import React from 'react'
-import { InputGroup,FormControl,Form } from "react-bootstrap";
-function Signup() {
-    return (
-        <div>
-             <form>
-                <h3>Sign Up</h3>
+import React,{useState} from 'react'
+import { InputGroup,FormControl,Form,Button } from "react-bootstrap";
+import { Redirect } from 'react-router-dom'
 
+const {
+    Stitch,
+    RemoteMongoClient,
+    AnonymousCredential
+} = require('mongodb-stitch-browser-sdk');
+
+const client = Stitch.initializeDefaultAppClient('elatic-somyc');
+
+function Signup() {
+
+const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('video');
+    client.auth.loginWithCredential(new AnonymousCredential()).then(user =>
+        db.collection('movieDetails').find({'year':1968}).asArray()
+      ).then(docs => {
+          console.log("Found docs", docs)
+      }).catch(err => {
+          console.error(err)
+      });
+
+    const [validated, setValidated] = useState(false);
+    const [RedirectVar, setRedirectVar] = useState(false)
+    const [PasswordValidation, setPasswordValidation] = useState(false)
+    const [SignInRedir, setSignInRedir] = useState(false)
+    const setValidationState=()=>{
+        if(PasswordValidation===true&&validated===true)
+         return true
+    }
+
+    const renderRedirect = () => {
+        if (RedirectVar) {
+            return <Redirect to='/dashboard' />
+        }
+        else if(SignInRedir){
+            return <Redirect to='/sign-in'/>
+        }
+    }
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        else{
+            setRedirectVar(true)
+        }
+        setValidated(true);
+    };
+
+    return (
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                 {renderRedirect()}
+                <h3>Sign Up</h3>
                     <InputGroup className="mb-3">
                     <InputGroup.Prepend>
                         <InputGroup.Text>Full name</InputGroup.Text>
                     </InputGroup.Prepend>
-                    <FormControl />
-                    <FormControl />
+                    <FormControl required/>
+                    <FormControl required/>
                     </InputGroup>
 
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control required type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control required type="password" placeholder="Password" />
+                    </Form.Group>
 
-                <button type="submit" className="btn btn-custom btn-block">Sign Up</button>
+                    <Button type="submit" disabled={setValidationState()} className="btn btn-custom btn-block">
+                        Submit
+                    </Button>
                 <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
+                    Already registered 
+                    <Button type="button" disabled={setValidationState()} className="btn btn-custom" onClick={()=>{setSignInRedir(true)}}>
+                        Sign in
+                    </Button>
                 </p>
-            </form>
-        </div>
+            </Form>
     )
 }
 
